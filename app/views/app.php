7c33e4b478c6cffb3e6803933b723c16e3c4d028
Pham Van A <phamvana@gmail.com>
2023-06-18 17:16:32 +0700

    add template

--- app/views/app.php
+++ app/views/app.php
@@ -1,18 +1,19 @@
-<?php require_once "./app/views/head.php";?>
-</head>
+<?php require_once "./app/views/pages/head.php"; ?>
+
 <body>
     <div id="header">
-    <?php require_once "./app/views/baner.php"; ?>
-    <h3>
-        <?php echo $data["title"] . " => " . $data["function"]; ?>
-    </h3>
+        <?php require_once "./app/views/pages/baner.php"; ?>
+        <h3>
+            <?php echo $data["title"]; ?>
+        </h3>
+
     </div>
     <div id="content">
         <?php
-        require_once "./app/views/pages/".$data["page"].".php";
-        
+        require_once "./app/views/pages/".$data["page"]. ".php";
         ?>
     </div>
-        <?php require_once "./app/views/footer.php"; ?>
+    <?php require_once "./app/views/pages/footer.php"; ?>
 </body>
+
 </html>
---
+++ app/views/pages/head.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>
+		<?php
+		echo $data["title"];
+		?>
+	</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="./app/views/Themes/images/icons/favicon.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/fonts/linearicons-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/vendor/slick/slick.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/vendor/MagnificPopup/magnific-popup.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href=".app/views/Themes/css/util.css">
+	<link rel="stylesheet" type="text/css" href="./app/views/Themes/css/main.css">
+<!--===============================================================================================-->
+
+	<!--Boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="/fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
+    <style>
+        div{padding:20px}
+        #header,#footer{background-color: aqua;}
+    </style>
+</head>
